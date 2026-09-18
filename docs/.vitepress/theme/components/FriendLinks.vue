6aac8baf136716e633d1eef611bcb2e612a38756
Cure2004 <1352862815@qq.com>
2026-09-18 16:19:25 +0800

    docs：更新友链

--- docs/.vitepress/theme/components/FriendLinks.vue
+++ docs/.vitepress/theme/components/FriendLinks.vue
@@ -8,9 +8,10 @@ interface FriendLink { name: string, image: string, url: string, description: st
 const links: FriendLink[] = [
 	{ name: '南开大学', image: '/img/10/10/南开大学WX头像.jpg', url: 'http://weixin.qq.com/r/mp/-3VFXTHEM2xMrXU_9yDt', description: '允公允能，日新月异。这里是南开大学官微，百年南开欢迎你~', tag: '校园媒体', qr: true },
 	{ name: 'NKUstudy', image: '/img/10/10/NK-study头像.jpg', url: 'https://nkustudy.top/', description: 'NKUStudy 是一个面向课程资料整理、课程导航与教师评价的轻量级站点，旨在帮助同学更方便地查找、整理和分享课程相关信息。', tag: '校友项目' },
-	{ name: 'NCEPUwiki', image: '/img/10/10/NCEPUwiki头像.svg', url: 'https://wiki.ncepuinfo.cc/', description: '华北电力大学学生共同维护的非官方校园知识库', tag: '兄弟院校' },
+	{ name: 'NCEPUwiki', image: '/img/10/10/NCEPUwiki头像.png', url: 'https://wiki.ncepuinfo.cc/', description: '华北电力大学学生共同维护的非官方校园知识库。把散落的经验，整理成下一位同学用得上的指南。', tag: '兄弟院校' },
 	{ name: 'NKUMC文档站', image: '/img/10/10/NKUMC文档站头像.png', url: 'https://docs.voxelnku.net/', description: '本站建立的目的是集中存放教程与Wiki类内容，助力新人了解游戏与社区，帮助新任社团干部快速学习社团运作与管理、掌握基本运维经验；同时为智能体南姬提供知识库语料，探索AI面向社区成员答疑解惑、节省人力的可能。', tag: '校友项目' },
 	{ name: '青理Wiki', image: '/img/10/10/青理Wiki头像.png', url: 'https://wiki.quters.top/', description: '青理生活指南(青理Wiki)是由青岛理工大学学生自发维护的一个面向青岛理工大学学生的生活指南，旨在为新生和在校生提供便捷的生活信息和资源。', tag: '兄弟院校' },
+	{ name: '西邮Wiki', image: '/img/10/10/西邮Wiki头像.png', url: 'https://wiki.cooo.site/', description: '《西邮 Wiki》是由校内智邮普创、Linux 兴趣小组、计科卓越、邮立方等多个校内实验室、非官方团体成员合作编写的校园生活指南，致力于为学生提供通透的校园见解，并将随着学校的发展持续更新。', tag: '兄弟院校' },
 ].map(link => ({ ...link, image: withBase(link.image), url: link.url.startsWith('/') ? withBase(link.url) : link.url }))
 </script>
 
